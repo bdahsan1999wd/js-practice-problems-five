a916aefb4c6fec36a2ce58f6b64d0497731ce943
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-09 20:53:12 +0600

    factorial-forLoop-Reverse added

---
+++ factorial-forLoop-Reverse.js
@@ -0,0 +1,40 @@
+/* --------------------multiplication by for loop--------------- */
+
+// function multiplicationOfNumbers(number){
+//     let result = 1;
+//     for(let i = 1; i <= number; i++){
+//         result = result * i;
+//         // console.log(result);
+//     }
+//     return result;
+// }
+// const result = multiplicationOfNumbers(7);
+// console.log(result);
+
+/* ------------------factorial by for loop----------------------- */
+
+// function factorial(number){
+//     let result = 1;
+//     for(let i = 1; i <= number; i++){
+//         result = result * i;
+//         // console.log(result);
+//     }
+//     return result;
+// }
+// const number = 7;
+// const result = factorial(number);
+// console.log(result);
+
+/* ---------------factorial reverse by for loop-------------- */
+
+// function factorial(number){
+//     let result = 1;
+//     for(let i = number; i >= 1; i--){
+//         result = result * i;
+//         // console.log(i);
+//     }
+//     return result;
+// }
+// const number = 7;
+// const reverse = factorial(number);
+// console.log('factorial of:', number, reverse);
