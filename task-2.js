1d224a48f712cf68dcbc33e6efbcf131f71f5916
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-19 16:52:51 +0600

    task-2 added

---
+++ task-2.js
@@ -0,0 +1,11 @@
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+console.log(car);
+
+// Add Passenger Capacity Property
+car.passengerCapacity = 5;
+
+console.log(car);
