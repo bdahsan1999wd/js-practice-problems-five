ee169d744693820e9c3f4fa91986cb896c440b09
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-19 16:58:39 +0600

    task-4 added

---
+++ task-4.js
@@ -0,0 +1,11 @@
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+// Get all keys of the object and count them
+const propertyCount = Object.keys(student).length;
+
+console.log(propertyCount);
