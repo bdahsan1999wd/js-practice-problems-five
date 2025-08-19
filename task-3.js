576d1377bacf1017a1081085c8b616b85884de1f
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-19 16:55:17 +0600

    task-3 added

---
+++ task-3.js
@@ -0,0 +1,14 @@
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+// Store physics marks in a variable
+const physicsMarks = student.physics.marks;
+
+console.log(physicsMarks);
