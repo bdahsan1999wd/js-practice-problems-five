5cc04d44b56d24c287b41a3dd86bdadc2f89bd2a
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-19 16:49:51 +0600

    task-1 added

---
+++ task-1.js
@@ -0,0 +1,11 @@
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": "#daa520"
+};
+
+// Access the "golden rod" value using bracket notation
+const goldenRodColor = colors["golden rod"];
+
+console.log(goldenRodColor);
