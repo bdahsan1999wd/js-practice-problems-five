c269521af546edc7170e47857df98f202cb2fc3b
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-20 15:13:35 +0600

    task-9 added

---
+++ task-9.js
@@ -0,0 +1,15 @@
+let movie = {
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    cast: [
+        "Matthew McConaughey",
+        "Anne Hathaway",
+        "Jessica Chastain",
+        "Michael Caine"
+    ]
+};
+
+// Get the total number of cast members using array length
+const castCount = movie.cast.length;
+
+console.log(castCount);
