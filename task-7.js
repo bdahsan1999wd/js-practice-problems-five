e8c5dd0762e4f7fd6bf3a01f81dd0b4d7cf2ef2d
bdahsan1999wd <bdahsan1999wd@gmail.com>
2025-08-20 15:09:22 +0600

    task-7 added

---
+++ task-7.js
@@ -0,0 +1,10 @@
+let screen = {
+    brand: "Samsung",
+    model: "Galaxy S22",
+    price: 80000
+};
+
+// Add a nested "dimensions" property with width and height
+screen.dimensions = { width: 100, height: 50 };
+
+console.log(screen);
